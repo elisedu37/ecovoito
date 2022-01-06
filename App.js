03b09086d91918cf640391def9d2dcbfc3bf3139
Balthazard Matéo <mateo.balthazard@hotmail.com>
2022-01-06 11:48:55 +0100

    test

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import React, {useEffect, useState } from 'React';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [userToken, setUserToken] = React.useState(null);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  if(isLoading) {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
