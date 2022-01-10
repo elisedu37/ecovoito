d8d7b3fd9398c670a5e67d0d48ff169a14909a58
lucas tomas <lucas.tomas43@gmail.com>
2022-01-10 11:08:46 +0100

    [-]clean

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
-import React, {useEffect, useState } from 'React';
-import { StatusBar } from 'expo-status-bar';
+import React from 'React';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [userToken, setUserToken] = React.useState(null);
 
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, []);
-
-  if(isLoading) {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-}
+
 
 const styles = StyleSheet.create({
   container: {
--- components/QRCodeGenerator.js
+++ components/QRCodeGenerator.js
@@ -1,13 +1,6 @@
 import { Text, View, StyleSheet, Linking } from 'react-native';
 import SvgQRCode from 'react-native-qrcode-svg';
-import useCachedResources from "../hooks/useCachedResources";
-import useColorScheme from "../hooks/useColorScheme";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import Navigation from "../navigation";
-import {StatusBar} from "expo-status-bar";
 
-// $ npm install --save react-router
-// $ npm install --save react-router-redux
 
 export default function QRCodeGenerator() {
     return (
