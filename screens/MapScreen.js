94960d45afcb385af78ba65d4a32bbd285e2350d
elise <elise37@sfr.fr>
2022-02-11 16:59:15 +0100

    maps config + initial region

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -7,7 +7,15 @@ export default function MapScreen() {
 
   return (
     <View style={styles.container}>
-    <MapView  style={styles.map} /> 
+     <MapView
+     style={styles.map}
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
   </View>  );
 }
 
