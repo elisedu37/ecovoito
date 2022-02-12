f26e99acfc1538dad016d480a0c38d57cab994c9
elise <elise37@sfr.fr>
2022-02-12 15:14:40 +0100

    map direction entre 2 localisations, error api key

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {  Text, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
+import { Marker, Polyline } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function MapScreen() {
-  
+  const [coordinates] = useState([
+    {
+      latitude: 48.8587741,
+      longitude: 2.2069771,
+    },
+    {
+      latitude: 48.8323785,
+      longitude: 2.3361663,
+    },
+  ]);
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyA_ovxwcIN3moaaJ97gQCDA3m0wTOLmMBs';
 
   return (
     <View style={styles.container}>
-     <MapView
-     style={styles.map}
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
-  </View>  );
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: coordinates[0].latitude, 
+          longitude: coordinates[0].longitude,
+          latitudeDelta: 0.0622,
+          longitudeDelta: 0.0121,
+        }}>
+           <MapViewDirections
+          origin={coordinates[0]}
+          destination={coordinates[1]}
+          apikey={GOOGLE_MAPS_APIKEY} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#111111"
+        />
+        <Marker coordinate={coordinates[0]} />
+        <Marker coordinate={coordinates[1]} />
+        
+      </MapView>
+    </View> );
 }
 
 const styles = StyleSheet.create({
