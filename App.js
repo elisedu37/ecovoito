8cc3f73060dde76d18551749d99f0b16b58bd2f2
lucas tomas <lucas.tomas43@gmail.com>
2022-03-07 08:27:59 +0100

    [+] Push notification onclick -> il faudrait que les notifs soient push en auto

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Button, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ScanQRCode from "./screens/ScanQRCode";
@@ -19,10 +19,18 @@ import Trajets from './screens/Trajets';
 import Analyse from './screens/Analyse';
 import Profil from './screens/Profil';
 import setRole from './forms/setRole';
+import Constants from 'expo-constants';
 import TrajetIndividuel from './screens/TrajetIndividuel';
-
+import * as Notifications from 'expo-notifications';
 const Stack = createNativeStackNavigator();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const MyStack = () => {
   const user = auth.currentUser;
@@ -59,9 +67,96 @@ const MyStack = () => {
     </>
   );
 };
-export default MyStack;
-  
-  
+// export default MyStack;
+
+export default function PushAnImage() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+      <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'space-around',
+          }}>
+        {/*<Text>Your expo push token: {expoPushToken}</Text>*/}
+        {/*<View style={{ alignItems: 'center', justifyContent: 'center' }}>*/}
+        {/*  <Text>Title: {notification && notification.request.content.title} </Text>*/}
+        {/*  <Text>Body: {notification && notification.request.content.body}</Text>*/}
+        {/*  <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>*/}
+        {/*</View>*/}
+        <Button
+            title="Notif"
+            onPress={async () => {
+              await schedulePushNotification();
+            }}
+        />
+
+      </View>
+  );
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Trajet bien créé",
+      body: 'Pensez à renseigner votre trajet du jour',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 
 const styles = StyleSheet.create({
